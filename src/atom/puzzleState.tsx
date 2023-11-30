a0327257f3797094c4c1c90d1e6430a525a4ea0f
Ksechan <rlatpcks77@gamil.com>
2023-11-30 17:19:04 +0900

    [feat] add recoilstate atom

---
+++ src/atom/puzzleState.tsx
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+export const puzzleImageState = atom<string | null>({
+  key: "puzzleImageState",
+  default: null,
+});
+
+export const puzzleCompleteState = atom<boolean>({
+  key: "puzzleComplete",
+  default: false,
+});
+
+export const puzzleState = atom<{ index: number; dataUrl: string }[]>({
+  key: "puzzleState",
+  default: [],
+});
