602164ba6aa9c83b87626df4b1cdb3d318305bce
Ksechan <rlatpcks77@gmail.com>
2023-12-04 00:18:29 +0900

    [feat] add touchscreen function

--- src/util/index.tsx
+++ src/util/index.tsx
@@ -16,3 +16,31 @@ export const resizeImage = (
     img.src = image as string;
   });
 };
+
+export const isTouchScreen = window.matchMedia(
+  "(hover: none) and (pointer: coarse)"
+).matches;
+
+// 마우스 움직임 변화를 측정하는 유틸
+export const getDelta = (
+  startEvent: MouseEvent | TouchEvent,
+  moveEvent: MouseEvent | TouchEvent
+) => {
+  if (isTouchScreen) {
+    const start = startEvent as TouchEvent;
+    const move = moveEvent as TouchEvent;
+
+    return {
+      deltaX: move.touches[0].pageX - start.touches[0].pageX,
+      deltaY: move.touches[0].pageY - start.touches[0].pageY,
+    };
+  }
+
+  const start = startEvent as MouseEvent;
+  const move = moveEvent as MouseEvent;
+
+  return {
+    deltaX: move.pageX - start.pageX,
+    deltaY: move.pageY - start.pageY,
+  };
+};
