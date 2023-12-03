9faa358364f997f9c5ddac7d3048d7c6d245b425
Ksechan <rlatpcks77@gmail.com>
2023-12-04 00:17:59 +0900

    [feat] add PuzzleContainer css

--- src/components/PuzzleContainer/index.tsx
+++ src/components/PuzzleContainer/index.tsx
@@ -7,38 +7,42 @@ import {
 } from "../../atom/puzzleState";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import PuzzleList from "../PuzzleList";
+import Header from "../Header";
 
 const PuzzleContainer = () => {
   const value = useRecoilValue(puzzleState);
-  const dragValue = useRecoilValue(puzzleDragState);
-  const dragComplete = useSetRecoilState(puzzleDragCompleteState);
-
-  const onDragDrop = (index: number, event: DragEvent<HTMLImageElement>) => {
-    const element = event.target as HTMLImageElement;
-    if (dragValue.index - 1 === index) {
-      element.setAttribute("src", dragValue.dataUrl);
-      dragComplete(true);
-    }
-  };
+  // const dragValue = useRecoilValue(puzzleDragState);
+  // const dragComplete = useSetRecoilState(puzzleDragCompleteState);
 
   return (
-    <>
-      <Styled.Container>
-        {[...Array(9).keys()].map((key, index) => {
-          return (
-            <Styled.Content
-              className={`dnd-drop-area ${index}`}
-              key={index}
-              onDrop={(event) => {
-                onDragDrop(index, event);
-              }}
-              onDragOver={(e) => e.preventDefault()}
-            />
-          );
-        })}
-        {value.length > 0 && <PuzzleList />}
-      </Styled.Container>
-    </>
+    <Styled.Container>
+      <Header />
+      <Styled.PuzzleAreaWrap>
+        <Styled.PuzzleArea>
+          {[...Array(9).keys()].map((key, index) => {
+            return (
+              <Styled.Content
+                className={`dnd-drop-area ${index}`}
+                key={index}
+              />
+            );
+          })}
+        </Styled.PuzzleArea>
+        <Styled.Title>퍼즐 조각</Styled.Title>
+        <Styled.subtitle>
+          조각을 꾹 눌러서 위 퍼즐판에 넣고 뺄 수 있어요!
+        </Styled.subtitle>
+      </Styled.PuzzleAreaWrap>
+      {value.length > 0 && (
+        <>
+          <PuzzleList />
+          <Styled.ButtonWrap>
+            <Styled.ListByeButton>이 사진 그만 볼래요</Styled.ListByeButton>
+            <Styled.ListPassButton>넘기기</Styled.ListPassButton>
+          </Styled.ButtonWrap>
+        </>
+      )}
+    </Styled.Container>
   );
 };
 
--- src/components/PuzzleContainer/style.tsx
+++ src/components/PuzzleContainer/style.tsx
@@ -1,15 +1,81 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const PuzzleAreaWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+
+export const PuzzleArea = styled.div`
   background-color: #cccccc;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr;
-  width: 500px;
+  width: 363px;
+  border-radius: 16px;
+  overflow: hidden;
 `;
 
 export const Content = styled.img`
-  background-color: #ffffff;
-  width: 70px;
-  height: 70px;
+  background-color: #e9e9e9;
+  width: 121px;
+  height: 121px;
+  border: 1px solid #ffffff;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Title = styled.p`
+  font-size: 24px;
+  font-weight: 700;
+  margin: 32px 0 16px;
+`;
+
+export const subtitle = styled.p`
+  margin-bottom: 24px;
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  width: 363px;
+`;
+
+export const ListByeButton = styled.button`
+  padding: 8px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  background-color: #ffaa00;
+  border: none;
+  border-radius: 100px;
+  margin-right: 16px;
+  flex: 2;
+  font-weight: 700;
+  font-size: 16px;
+  color: #ffffff;
+`;
+
+export const ListPassButton = styled.button`
+  padding: 8px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  background-color: #ffaa00;
+  border: none;
+  border-radius: 100px;
+  flex: 1;
+  font-weight: 700;
+  font-size: 16px;
+  color: #ffffff;
 `;
