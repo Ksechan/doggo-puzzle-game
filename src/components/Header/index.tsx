fec9b476a6e7eda5dae9c6edcb3c30ac25a1ddab
Ksechan <rlatpcks77@gmail.com>
2023-12-04 00:16:09 +0900

    [feat] add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import * as Styled from "./style";
+import close from "../../assets/close.svg";
+import refresh from "../../assets/refresh.svg";
+
+const Header = () => {
+  return (
+    <Styled.Container>
+      <Styled.LeftWrap>
+        <Styled.Icon src={close} />
+      </Styled.LeftWrap>
+      <Styled.RightWrap>
+        <Styled.Icon src={refresh} />
+      </Styled.RightWrap>
+    </Styled.Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/style.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 363px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 30px 20px;
+`;
+
+export const LeftWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const RightWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+`;
