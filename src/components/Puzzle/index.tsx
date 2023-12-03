f55a518b2bd3b58a1d9fada8bbb8881a63281ef7
Ksechan <rlatpcks77@gmail.com>
2023-12-04 00:17:26 +0900

    [fix] fix ImageUploader visible

--- src/components/Puzzle/index.tsx
+++ src/components/Puzzle/index.tsx
@@ -1,13 +1,16 @@
 import PuzzleContainer from "../PuzzleContainer";
 import ImageUploader from "../ImageUploader";
 import PuzzleList from "../PuzzleList";
+import { puzzleState } from "../../atom/puzzleState";
 import * as Styled from "./style";
+import { useRecoilValue } from "recoil";
 
 const Puzzle = () => {
+  const puzzleValue = useRecoilValue(puzzleState);
   return (
     <Styled.Container>
       <PuzzleContainer />
-      <ImageUploader />
+      {puzzleValue.length === 0 && <ImageUploader />}
     </Styled.Container>
   );
 };
