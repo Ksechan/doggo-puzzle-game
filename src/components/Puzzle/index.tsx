fa5703cc0cc58860e00b565125528fa7ac82dd06
Ksechan <rlatpcks77@gmail.com>
2023-12-05 00:46:46 +0900

    [feat] add responsive size

--- src/components/Puzzle/index.tsx
+++ src/components/Puzzle/index.tsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from "react";
 import PuzzleContainer from "../PuzzleContainer";
 import ImageUploader from "../ImageUploader";
 import { puzzleCompleteState, puzzleState } from "../../atom/puzzleState";
 import * as Styled from "./style";
 import { useRecoilValue } from "recoil";
+import { useMediaQuery } from "react-responsive";
 
 const Puzzle = () => {
   const puzzleCompleteValue = useRecoilValue(puzzleCompleteState);
   const puzzleValue = useRecoilValue(puzzleState);
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
 
   return (
     <Styled.Container>
       <PuzzleContainer />
       {!puzzleCompleteValue && puzzleValue.length === 0 ? (
-        <ImageUploader />
+        <ImageUploader
+          width={isDesktop ? 480 : 345}
+          height={isDesktop ? 480 : 345}
+        />
       ) : null}
     </Styled.Container>
   );
