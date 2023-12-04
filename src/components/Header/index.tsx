5c515b822292e1c45cbfc05a14c8628a0fee90a7
Ksechan <rlatpcks77@gamil.com>
2023-12-04 17:13:33 +0900

    [feat] add refresh

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <Styled.LeftWrap>
         <Styled.Icon src={close} />
       </Styled.LeftWrap>
-      <Styled.RightWrap>
+      <Styled.RightWrap onClick={() => location.reload()}>
         <Styled.Icon src={refresh} />
       </Styled.RightWrap>
     </Styled.Container>
